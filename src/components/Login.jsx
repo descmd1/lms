1a5ab0942dd3e607f32a1bbcd573ffc3b7207525
descmd1 <aondoakaachris@gmail.co>
2025-10-01 16:44:25 +0100

    edited login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -53,7 +53,7 @@ export function Login() {
                 <button
                     type="button"
                     onClick={() => setShowPassword(!showPassword)} 
-                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 transition-colors duration-200"
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 transition-colors duration-200 vertical-center"
                 >
                     {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
                 </button>
